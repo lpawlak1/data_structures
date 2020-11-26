a352356467366f51b7048fcd4f77ce880dee7f8c
lpawlak1 <lukaszpawlak162@gmail.com>
2020-11-26 16:51:30 +0100

    Template and simple tests for stack.h

--- tests/stack_test.cpp
+++ tests/stack_test.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 
 int main(){
-    stack<int>* st = new stack<int>();
+    auto* st = new stack<int>();
     for (size_t i = 0; i < 1000; i++)
     {
-        st->push(123);
+        st->push(i+1);
     }
     for (size_t i = 0; i < 100; i++)
     {
         st->pop();
     }
+    printf("%d\n",st->peek());
     printf("%d\n",st->size());
     st->clear();
     if(st->empty()) printf("empty..\n");
 }
-
