c815f38f5aebefe395d36fd349086c75d2254739
lpawlak1 <lukaszpawlak162@gmail.com>
2020-12-15 15:52:21 +0100

    link_vector test

--- tests/link_gtest.cpp
+++ tests/link_gtest.cpp
@@ -5,7 +5,7 @@
 #include "gtest/gtest.h"
 #include "link_vector.h"
 
-link_vector<int> prepare(){
+link_vector<int> prepare_link(){
     link_vector<int> linkVector;
     for(auto i = 0; i < 100; i+=1){
         linkVector.push_back(i);
@@ -23,16 +23,16 @@ TEST(link_vector_test, push_test){
     EXPECT_EQ(200,linkVector.size());
 }
 TEST(link_vector_test, pop_get_test){
-    link_vector<int> linkVector = prepare();
-    EXPECT_EQ(99,linkVector.get(linkVector.size()-1));
-    EXPECT_EQ(0,linkVector.get(0));
+    link_vector<int> linkVector = prepare_link();
+    EXPECT_EQ(99,linkVector[linkVector.size()-1]);
+    EXPECT_EQ(0,linkVector[0]);
     EXPECT_EQ(99,linkVector.pop_back());
     EXPECT_EQ(0,linkVector.pop_front());
     EXPECT_EQ(1,linkVector.pop(0));
     EXPECT_EQ(97,linkVector.size());
 }
 TEST(link_vector_test, clear_test){
-    link_vector<int> linkVector = prepare();
+    link_vector<int> linkVector = prepare_link();
     linkVector.clear();
     EXPECT_TRUE(linkVector.empty());
     EXPECT_EQ(0,linkVector.size());
---
+++ tests/source_gtest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by lukas on 28.11.2020.
+//
+#include "gtest/gtest.h"
+#include "link_gtest.h"
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
